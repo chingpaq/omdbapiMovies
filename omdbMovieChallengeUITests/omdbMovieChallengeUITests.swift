ac611685a3d42283e66baa41f7071a03b019301f
Ching Parungao <mbparungao@gmail.com>
2021-01-21 14:30:33 +0800

    Tests improvement

--- omdbMovieChallengeUITests/omdbMovieChallengeUITests.swift
+++ omdbMovieChallengeUITests/omdbMovieChallengeUITests.swift
@@ -9,34 +9,16 @@ import XCTest
 
 class omdbMovieChallengeUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
     func testIfAvengersMovieAvailable() {
         let app = XCUIApplication()
+        
+        let movieTitle = "The Stubbed Avengers"
+        let textToSearchInDetailView = "Marvel Studios"
+        
         app.launch()
-        XCTAssert(app.tables.cells.staticTexts["The Stubbed Avengers"].waitForExistence(timeout: 12))
-        app.tables.cells.staticTexts["The Stubbed Avengers"].tap()
-        XCTAssert(app.staticTexts["Marvel Studios"].waitForExistence(timeout: 12))
+        XCTAssert(app.tables.cells.staticTexts[movieTitle].waitForExistence(timeout: 10))
+        app.tables.cells.staticTexts[movieTitle].tap()
+        XCTAssert(app.staticTexts[textToSearchInDetailView].waitForExistence(timeout: 10))
     }
     
 }
